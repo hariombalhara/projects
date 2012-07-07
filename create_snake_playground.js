a05b8443d4fd64cc27b3144a055359f7df9ec8b5
Hariom Balhara <hariombalhara@gmail.com>
2012-07-07 10:02:29 -0400

    Revamped

---
+++ create_snake_playground.js
@@ -0,0 +1,32 @@
+(function()
+{
+var  snake_playground=document.getElementById('snake_playground');
+if(snake_playground)
+{
+ if(snake_playground.confirmation_pending)
+ return;
+ snake_playground.confirmation_pending=true;
+ var response=confirm('Game is already running.Restart the game?');
+ snake_playground.confirmation_pending=false;
+ if(response)
+ {
+  document.body.removeChild(snake_playground);    
+ }
+ else
+ return;
+}
+ 
+ var sscript=document.createElement('script');
+ sscript.type="text/javascript";
+ sscript.id="snake_script";
+ sscript.src="https://raw.github.com/hariombalhara/snake/master/snake.js";
+  document.getElementsByTagName('head')[0].appendChild(sscript);
+ 
+ var sstyle=document.createElement('link');
+ sstyle.href="https://raw.github.com/hariombalhara/snake/master/snake.css";
+ sstyle.type="text/css";
+ sstyle.rel="stylesheet";
+ if(!document.querySelector('[src="'+sstyle.href+'"]'))
+ document.getElementsByTagName('head')[0].appendChild(sstyle);
+
+})();
---
+++ process.php
@@ -0,0 +1,5 @@
+<?php
+// We'll be outputting a PDF
+header('Content-type: text/html');
+echo "hariom"."parveen"
+?>
