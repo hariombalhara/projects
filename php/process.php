622d5b929e58737a43c7d60a27b8224d0bbfef8f
Hariom Balhara <hariombalhara@gmail.com>
2012-07-08 09:53:12 -0400

    Testing

--- php/process.php
+++ php/process.php
@@ -2,35 +2,35 @@
 header('Access-Control-Allow-Origin:*');
 header('Content-Type:text/html');
 $uuid = $_COOKIE['identifier'];
-echo $uuid;
+//echo $uuid;
 $host = $_ENV['OPENSHIFT_DB_HOST'];
 $pass = $_ENV['OPENSHIFT_DB_PASSWORD'];
 $username = $_ENV['OPENSHIFT_DB_USERNAME'];
 $port = $_ENV['OPENSHIFT_DB_PORT'];
 $conn = mysql_connect($host.":".$port,$username,$pass);
-$name = $_GET['name'];
+$name = $_GET['name']; //TODO CHANGE TO POST
 $email = $_GET['email'];
 if(!$conn)
 {
     die('Error Connecting:'.mysql_error());
 }
 mysql_select_db('php',$conn);
-echo $conn;
+//echo $conn;
 if(isset($uuid)) {
   $query = 'Select * from `snake` where `sessionId` = "'.$uuid.'"';
-  echo $query;
+  //echo $query;
   $res = mysql_query($query) or die(mysql_error());
   $row = mysql_fetch_array($res,MYSQL_BOTH);
-  print_r($row);
-  echo 'Email Id is'.$row['email'];
+  //print_r($row);
+  echo $row['email'];
 } else {
    global $uuid;
    global $name;
    global $email;
    $uuid = uniqid();
    $query = "Insert into `snake` (`email`,`sessionId`,`username`) values ('".$email."','".$uuid."','".$name."')";
-   echo $query;
+   //echo $query;
    $res = mysql_query($query) or die(mysql_error());
-   echo "Insert ".$res;
+   //echo "Insert ".$res;
 }
 ?>
