4af8ef479cb6e2f8623bcc543df238b5ca9888a6
Hariom Balhara <hariombalhara@gmail.com>
2012-07-08 08:17:26 -0400

    Testing

--- php/process.php
+++ php/process.php
@@ -1,13 +1,1 @@
-<?php
-echo "hariom";
-header('Access-Control-Allow-Origin:*');
-$uuid = $_COOKIE['identifier'];
-echo 'Host is'.OPENSHIFT_DB_HOST;
-if(isset($uuid)) {
-    //RTRV;
-} else {
-    $_POST[]
-    $uuid = uniqid();
-}
-
-?>
+dsfsdf
