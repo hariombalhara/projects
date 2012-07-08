efd9ef0d923bd75efcda7d46da515754dfc58120
Hariom Balhara <hariombalhara@gmail.com>
2012-07-08 08:44:41 -0400

    Testing

--- php/process.php
+++ php/process.php
@@ -2,7 +2,17 @@
 header('Access-Control-Allow-Origin:*');
 header('Content-Type:text/html');
 $uuid = $_COOKIE['identifier'];
-echo 'Host is'.$_ENV['OPENSHIFT_DB_HOST'];
+$host = $_ENV['OPENSHIFT_DB_HOST'];
+$pass = $_ENV['OPENSHIFT_DB_PASSWORD'];
+$username = $_ENV['OPENSHIFT_DB_USERNAME'];
+$port = $_ENV['OPENSHIFT_DB_PORT'];
+$conn = mysql_connect($host.":"$port,$username,$pass);
+echo $conn;
+if(!$conn)
+{
+    die('Error Connecting:'.mysql_error());
+}
+mysql_select_db('php',$conn);
 if(isset($uuid)) {
    //RTRV;
 } else {
