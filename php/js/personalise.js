e33fa7637406472b4f9de71b123cca8f86e5e7bf
Hariom Balhara <hariombalhara@gmail.com>
2012-07-12 02:40:29 -0400

    Tsting

--- php/js/personalise.js
+++ php/js/personalise.js
@@ -32,7 +32,7 @@ function gotAssertion(assertion) {
     if(!uid) {
         xmlhttp.open('POST','https://browserid.org/verify',true);
         xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send('assertion='+assertion+'&audience='+location.host);//location.host should be changed to hardcoded string.Its not safe.
+        xmlhttp.send('assertion='+assertion+'&audience=php-hariombalhara.rhcloud.com');//location.host should be changed to hardcoded string.Its not safe.
         xmlhttp.onreadystatechange = function() {
         if(xmlhttp.status === 200 && xmlhttp.readyState === 4) {
             var verified_obj = JSON.parse(xmlhttp.responseText);
